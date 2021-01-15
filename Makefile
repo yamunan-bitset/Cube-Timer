e32f9ce6f70635bb3814d8a6ec0bdea0d9c81b9b
Anirudh <stud81752@berlin-international-school.de>
2021-01-15 11:59:23 +0000

    made install configurations

---
+++ Makefile
@@ -0,0 +1,5 @@
+OUT = /usr/bin/cube-timer
+
+install:
+	sudo cp main.py $(OUT)
+	sudo chmod 777  $(OUT)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from time import sleep
 from tkinter import *
 from random import randint
